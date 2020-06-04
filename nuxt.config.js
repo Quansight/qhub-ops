cfe7d82fe0a2503b56669192e5648ddae00c9fe9
anirrudh <anik597@gmail.com>
2020-06-04 14:29:05 -0500

    fix routing

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,13 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/<repository-name>/'
+  }
+} : {}
+
+export default {
+  ...routerBase
+}
+
 export default {
   buildModules: [
     '@nuxtjs/markdownit',
